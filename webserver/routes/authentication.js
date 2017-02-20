bb87fdc65fcdfc23f77e00042f08cb75125565d8
ahuertam <ahuertam@yahoo.es>
2017-02-20 16:27:39 +0100

    changes Routes

--- webserver/routes/authentication.js
+++ webserver/routes/authentication.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router  = express.Router();
 
+router.get('/login', (req, res) => {
+    res.render('authentication/login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('authentication/signup');
+});
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect : '../views/denuncias/show',
+  failureRedirect : '/signup'
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect : '../views/denuncias/show',
+  failureRedirect : '/login'
+}));
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 module.exports = router;
