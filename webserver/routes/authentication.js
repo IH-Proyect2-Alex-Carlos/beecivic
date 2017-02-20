18b6d939d0075875ec202340bb9db53848b2df24
ahuertam <ahuertam@yahoo.es>
2017-02-20 16:39:05 +0100

    initial routes working

--- webserver/routes/authentication.js
+++ webserver/routes/authentication.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const passport      = require("passport");
 
 router.get('/login', (req, res) => {
     res.render('authentication/login');
